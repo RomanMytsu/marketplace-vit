dc0692f544d8ee5926c4142bf516b7866920d7cf
Roman <romanmytsu@gmail.com>
2026-05-23 18:06:49 +0300

    init

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/markerplace-vit/",
+  base: "/marketplace-vit/",
   plugins: [react()],
   resolve: {
     alias: {
